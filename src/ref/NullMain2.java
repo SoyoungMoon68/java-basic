ffc3f78ad43898d36e73756b5496b4f9ae1855a3
soyoungmoon <dalso51166868@gmail.com>
2025-06-01 11:54:12 +0900

    feat: 기본형 vs 참조형

---
+++ src/ref/NullMain2.java
@@ -0,0 +1,9 @@
+package ref;
+
+public class NullMain2 {
+    public static void main(String[] args) {
+        Data data = null;
+        //null.value = 10; //null pointer exception 예외 발생
+        System.out.println("data = " + data);
+    }
+}
